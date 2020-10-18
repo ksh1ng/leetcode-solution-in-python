ed7c2ac6fc4249c1f7cac80e416e5e4b41b120c1
ksh1ng <38285921+ksh1ng@users.noreply.github.com>
2020-10-18 16:29:09 +0800

    Add files via upload

---
+++ 001_two_sum.py
@@ -0,0 +1,29 @@
+'''
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+
+Example
+    Input: nums = [2,7,11,15], target = 9
+    Output: [0,1]
+    Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+'''
+
+class Solution(object):
+
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype two_indices: List[int]
+        """
+        for i in range(len(nums)):
+            num1 = nums[i]
+            num2 = target - nums[i]
+            if num2 in nums[i+1:]:
+                return [i, nums.index(num2, i+1)]
+
+
+if __name__ == "__main__":
+
+    print(Solution().twoSum(nums=[2,7,11,15], target=9))
